65671006b36c04cf64283c4b215c3f431aae7b52
tomohikoseven <tomohikoseven0905@gmail.com>
2022-03-06 10:53:51 +0900

    Option APIでのカスタムディレクティブを実装.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,44 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div id="simplest-directiv-example" class="demo">
+    <input />
+    <input v-focus />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+// import  myfocus from "@/directives/cuctom-focus.vue";
 
 export default defineComponent({
   name: "About",
+  // directives: {
+  //   myfocus,
+  // },
   setup() {
     return {};
   },
+  directives: {
+    focus: {
+      // directive definition
+      mounted(el) {
+        el.focus();
+      },
+    },
+  },
 });
 </script>
+
+<style>
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
+</style>
