a0ca2fb9ee75f1ddd600adc736ab63852df30c2e
tomohikoseven <tomohikoseven0905@gmail.com>
2022-04-03 18:06:44 +0900

    input項目にfilterをしようとしたけど、できなかった。

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(store).use(router);
+// app.config.globalProperties.$filters = {
+//   formatNumber(num: string) {
+//     return "$" + num;
+//   },
+// };
+app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,31 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div>
+    aaa
+    <input type="text" :value="filtered" @input="tempText = $event.target.value" />
+  </div>
+  <div>
+    {{ tempText }}
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, computed } from "vue";
 
 export default defineComponent({
   name: "About",
   setup() {
-    return {};
+    const filterTest = ref("");
+    const tempText = ref("");
+    const filtered = computed(() => {
+      return "$" + tempText.value;
+    });
+    return {
+      filterTest,
+      filtered,
+      tempText,
+    };
   },
 });
 </script>
